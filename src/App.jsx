39c5c0d8042826dce294ada527c6c91f22fe47a5
Namiajohnsi <160624421+Namiajohnsi@users.noreply.github.com>
2025-10-13 06:09:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import products from '../products.json';
 import { useState } from "react";
 import Home from "./pages/Home";
 import ProductDetails from "./components/ProductDetails";
-import products from "./data/products";
+//import products from "./data/products";
 
 export default function App() {
   const [selectedProductId, setSelectedProductId] = useState(null);
